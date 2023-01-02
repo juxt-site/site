49695792e921db59c69618cb1c789440f17b0b8e
Malcolm Sparks <mal@juxt.pro>
2023-01-02 15:49:34 +0000

    Add a new test for the system-api package

--- test/juxt/site/system_api_test.clj
+++ test/juxt/site/system_api_test.clj
@@ -8,138 +8,85 @@
    [juxt.site.repl :as repl]
    [juxt.site.test-helpers.login :as login]
    [juxt.site.test-helpers.oauth :as oauth]
-   [xtdb.api :as xt]
    [juxt.test.util
     :refer [with-system-xt
             with-session-token with-bearer-token
-            with-fixtures *handler* *xt-node* with-handler
-            install-package! install-resource-with-action!]]))
+            with-fixtures *handler* with-handler
+            install-packages!
+            install-resource-with-action!]]))
 
 (use-fixtures :each with-system-xt with-handler)
 
-;;deftest system-api-test
-
-#_(with-fixtures
+(deftest system-api-test
 
   ;; Build authorization server
-  (let [uri-map {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"}]
-    (install-package! "bootstrap" uri-map))
+  (install-packages!
+   ["bootstrap"]
+   {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"})
 
   ;; Build resource server
-  (let [uri-map {#{"https://auth.example.org" "https://core.example.org"} "https://auth.example.test"
-                 "https://example.org" "https://data.example.test"}]
-    (install-package! "protection-spaces" uri-map)
-    (install-package! "system-api" uri-map))
+  (install-packages!
+   ["protection-spaces" "system-api"]
+   {#{"https://auth.example.org" "https://core.example.org"} "https://auth.example.test"
+    "https://example.org" "https://data.example.test"})
 
-  (let [response
-        (*handler*
-         {:juxt.site/uri "https://data.example.test/_site/actions.json"
-          :ring.request/method :get})]
-    (cond->
-        (select-keys
-         response
-         [:ring.response/status
-          :ring.response/body
-          :ring.response/headers])
-      (= "application/json" (get-in response [:ring.response/headers "content-type"]))
-      (update :ring.response/body json/read-value)))
+  ;; Test to ensure we can't access https://data.example.test/_site/actions.json
+  (with-logging
+    (let [response
+          (*handler*
+           {:juxt.site/uri "https://data.example.test/_site/actions.json"
+            :ring.request/method :get})]
+      (is (= 401 (:ring.response/status response)))
+      (is (= "Bearer" (get-in response [:ring.response/headers "www-authenticate"])))))
 
-  (let [response
-        (*handler*
-         {:juxt.site/uri "https://data.example.test/_site/actions.html"
-          :ring.request/method :get})]
-    (cond->
-        (select-keys
-         response
-         [:ring.response/status
-          :ring.response/body
-          :ring.response/headers])
-      (= "application/json" (get-in response [:ring.response/headers "content-type"]))
-      (update :ring.response/body json/read-value)))
-
-
-  ;; Register an application
-  ;; TODO: Only temporary while moving init below pkg
-  #_(install-resource-with-action!
-     {:juxt.site/subject-id "https://auth.example.test/_site/subjects/system"
-      :juxt.site/action-id "https://auth.example.test/actions/register-client"
-      :juxt.site/input
-      {:juxt.site/client-id "test-app"
-       :juxt.site/client-type "confidential"
-       :juxt.site/redirect-uri "https://test-app.example.test/callback"}})
-
-  ;; Now we need some mechanism to authenticate with the authorization server in
-  ;; order to authorize applications and acquire tokens.
-
-  #_#_#_#_#_#_(install-package!
-               "login-form"
-               {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"})
-
-  (install-package!
-   "user-model"
+  (install-packages!
+   ["sessions"
+    "oauth-authorization-server"
+    "login-form"
+    "user-model"
+    "password-based-user-identity"
+    "example-users"]
    {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"})
 
-  (install-package!
-   "password-based-user-identity"
-   {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"})
+  (install-resource-with-action!
+   {:juxt.site/subject-id "https://auth.example.test/_site/subjects/system"
+    :juxt.site/action-id "https://auth.example.test/actions/register-client"
+    :juxt.site/input
+    {:juxt.site/client-id "test-app"
+     :juxt.site/client-type "confidential"
+     :juxt.site/redirect-uri "https://test-app.example.test/callback"}})
 
-  (install-package!
-   "example-users"
-   {#{"https://example.org" "https://core.example.org"} "https://auth.example.test"})
+  (let [login-result
+        (login/login-with-form!
+         *handler*
+         "username" "alice"
+         "password" "garden"
+         :juxt.site/uri "https://auth.example.test/login")
 
-  (install-package!
-   "protection-spaces"
-   {#{"https://auth.example.org" "https://core.example.org"} "https://auth.example.test"})
+        session-token (:juxt.site/session-token login-result)
+        _ (assert session-token)
 
-  (install-package!
-   "whoami"
-   {"https://example.org" "https://example.test"
-    #{"https://auth.example.org" "https://core.example.org"} "https://auth.example.test"})
+        {access-token "access_token"}
+        (with-session-token
+          session-token
+          (oauth/authorize!
+           "https://auth.example.test/oauth/authorize"
+           {"client_id" "test-app"}))]
 
-  #_(let [login-result
-          (login/login-with-form!
-           *handler*
-           "username" "alice"
-           "password" "garden"
-           :juxt.site/uri "https://auth.example.test/login")
+    (with-bearer-token access-token
+      (let [response
+            (*handler*
+             {:juxt.site/uri "https://data.example.test/_site/actions"
+              :ring.request/method :get
+              :ring.request/headers
+              {"accept" "application/json"}})]
 
-          session-token (:juxt.site/session-token login-result)
-          _ (assert session-token)
+        ;; This access token is not sufficient
+        (is (= 403 (:ring.response/status response)))
 
-          {access-token "access_token"}
-          (with-session-token
-            session-token
-            (oauth/authorize!
-             "https://auth.example.test/oauth/authorize"
-             {"client_id" "test-app"}))]
-
-      (with-bearer-token access-token
-        (let [{:ring.response/keys [headers body]}
-              (*handler*
-               {:juxt.site/uri "https://example.test/whoami"
-                :ring.request/method :get
-                :ring.request/headers
-                {"accept" "application/json"}})]
-
-          (is (= "Alice"
-                 (-> body
-                     json/read-value
-                     (get-in ["juxt.site/subject"
-                              "juxt.site/user-identity"
-                              "juxt.site/user"
-                              "name"
-                              ]))))
-          (is (= "application/json" (get headers "content-type")))
-          (is (= "https://example.test/whoami.json" (get headers "content-location"))))
-
-        (let [{:ring.response/keys [status headers]}
-              (*handler*
-               {:juxt.site/uri "https://example.test/whoami.html"
-                :ring.request/method :get
-                :ring.request/headers
-                {"authorization" (format "Bearer %s" access-token)
-                 }})]
-          (is (= 200 status))
-          (is (= "text/html;charset=utf-8" (get headers "content-type"))))))
-
-  )
+        #_(cond-> response
+          true (select-keys [:ring.response/status
+                             :ring.response/headers
+                             :ring.response/body])
+          (= "application/json" (get-in response [:ring.response/headers "content-type"]))
+          (update :ring.response/body json/read-value))))))
