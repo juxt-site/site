edda05ddf8044a3c15f07debecefcc6a8e17b280
Malcolm Sparks <mal@juxt.pro>
2023-03-02 09:30:56 +0000

    Add CORS headers to authorize endpoint
    This makes it possible to call the authorize endpoint via a browser
    script.

--- test/juxt/site/auth_server_test.clj
+++ test/juxt/site/auth_server_test.clj
@@ -18,58 +18,56 @@
 
 (use-fixtures :each system-xt-fixture handler-fixture)
 
-;;deftest introspection-test
+(deftest cors-headers
+ (install-resource-groups! ["juxt/site/bootstrap"] AUTH_SERVER {})
 
-;; Use this to test CORS headers with an authorization-server
-#_(with-fixtures
-  (install-resource-groups! ["juxt/site/bootstrap"] AUTH_SERVER {})
+ (install-resource-groups!
+  ["juxt/site/oauth-authorization-server"
+   "juxt/site/login-form"
+   "juxt/site/example-users"]
+  AUTH_SERVER
+  {"session-scope" "https://auth.example.test/session-scopes/form-login-session"
+   "keypair" "https://auth.example.test/keypairs/test-kp-123"})
 
-  (install-resource-groups!
-   ["juxt/site/oauth-authorization-server"
-    "juxt/site/login-form"
-    "juxt/site/example-users"]
-   AUTH_SERVER
-   {"session-scope" "https://auth.example.test/session-scopes/form-login-session"
-    "keypair" "https://auth.example.test/keypairs/test-kp-123"})
+ (install-resource-groups!
+  ["juxt/site/example-apps"]
+  AUTH_SERVER
+  {"client-type" "public"
+   "origin" "https://test-app.test.com"
+   "resource-server" "https://data.example.test"
+   "redirect-uri" "https://test-app.test.com/redirect.html"})
 
-  (install-resource-groups!
-   ["juxt/site/example-apps"]
-   AUTH_SERVER
-   {"client-type" "public"
-    "origin" "https://test-app.test.com"
-    "resource-server" "https://data.example.test"
-    "redirect-uri" "https://test-app.test.com/redirect.html"})
+ (let [login-result
+       (login/login-with-form!
+        *handler*
+        "username" "alice"
+        "password" "garden"
+        :juxt.site/uri "https://auth.example.test/login-with-form")
 
-  (let [login-result
-        (login/login-with-form!
-         *handler*
-         "username" "alice"
-         "password" "garden"
-         :juxt.site/uri "https://auth.example.test/login-with-form")
+       session-token (:juxt.site/session-token login-result)
+       _ (assert session-token)
 
-        session-token (:juxt.site/session-token login-result)
-        _ (assert session-token)
+       response (with-session-token session-token
+                  (oauth/authorize-response!
+                   "https://auth.example.test/oauth/authorize"
+                   {"client_id" "test-app"
+                    ;; TODO: Aren't we meant to put in a response-type or something?
+                    :state "123"}))]
 
-        {access-token "access_token"}
-        (with-session-token
-          session-token
-          (oauth/authorize!
-           "https://auth.example.test/oauth/authorize"
-           {"client_id" "test-app"}))]
+   (is (= 303 (:ring.response/status response)))
 
-    (*handler* {:juxt.site/uri "https://auth.example.test/.well-known/oauth-authorization-server"
-                :ring.request/method :get})
+   (is (= "https://test-app.test.com"
+          (get-in response [:ring.response/headers "access-control-allow-origin"])))
 
-    ;; Post this to token
-
-    )
-
-  )
+   ;; Post this to token
+   )
+ )
 
 ;; Metadata test
-;;with-fixtures
 
-(with-fixtures
+;;deftest introspection-test
+
+#_(with-fixtures
   (install-resource-groups! ["juxt/site/bootstrap"] AUTH_SERVER {})
   (install-resource-groups!
    ["juxt/site/oauth-authorization-server"]
@@ -92,4 +90,15 @@
 
     json
 
-    ))
+
+
+    )
+
+  (let [{:ring.response/keys [status headers body]}
+          (*handler* {:juxt.site/uri  "https://auth.example.test/.well-known/jwks.json"
+                      :ring.request/method :get})
+          json (json/read-value body)]
+      (assert (is (= 200 status)))
+      json
+
+      ))
