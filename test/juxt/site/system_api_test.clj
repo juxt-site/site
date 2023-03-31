337719b5837aa72d83b566883d3ea70f2b0ca0b2
Malcolm Sparks <mal@juxt.pro>
2023-03-31 19:35:56 +0200

    Shorten scope names

--- test/juxt/site/system_api_test.clj
+++ test/juxt/site/system_api_test.clj
@@ -143,11 +143,14 @@
 ;; TODO: Can a client-id be a URI?
 
 (deftest put-user-combinations-test
-  (let [login (fn [username]
-                (login/login-with-form!
-                 username
-                 (case username "alice" "garden" "bob" "walrus"))) ]
-
+  (letfn [(login [username]
+            (login/login-with-form!
+             username
+             (case username
+               "alice" "garden"
+               "bob" "walrus")))
+          (absolute-scope [scopes]
+            (set (map (fn [scope] (str "https://auth.example.test/scopes/" scope)) scopes)))]
     (are [username client grant-type requested-scope expected-status]
         (testing (cond-> (format "%s using %s with %s grant" username client grant-type)
                    requested-scope (str " requesting " (str/join ", " requested-scope)))
@@ -159,7 +162,7 @@
                           :token-uri "https://auth.example.test/oauth/token"
                           :session-token session-token
                           :client (str "https://auth.example.test/clients/" client)}
-                   requested-scope (assoc :scope requested-scope)))]
+                   requested-scope (assoc :scope (absolute-scope requested-scope))))]
             (oauth/with-bearer-token access-token
               (let [payload (.getBytes (pr-str {:xt/id "https://data.example.test/users/hannah"}))
                     request {:juxt.site/uri "https://data.example.test/_site/users"
@@ -169,118 +172,73 @@
                               "content-length" (str (count payload))}
                              :ring.request/body (io/input-stream payload)}
                     response (*handler* request)]
-
                 (is (= expected-status (:ring.response/status response)))))))
 
       ;; Alice has permission, and using a client with global-scope
-        "alice" "global-scope-app" "authorization_code" nil 200
-        "alice" "global-scope-app" "implicit" nil 200
+      "alice" "global-scope-app" "authorization_code" nil 200
+      "alice" "global-scope-app" "implicit" nil 200
 
-        ;; Alice has permission, requesting write scope from a client
-        ;; with global scope
-        "alice" "global-scope-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        200
+      ;; Alice has permission, requesting write scope from a client
+      ;; with global scope
+      "alice" "global-scope-app" "authorization_code" #{"system/read" "system/write"} 200
+      "alice" "global-scope-app" "implicit" #{"system/read" "system/write"} 200
 
-        "alice" "global-scope-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        200
+      ;; Alice has permission, but not requesting sufficient scope
+      ;; from a client with global scope means she cannot create a
+      ;; user.
+      "alice" "global-scope-app" "authorization_code" #{"system/read"} 403
+      "alice" "global-scope-app" "implicit" #{"system/read"} 403
 
-        ;; Alice has permission, but not requesting sufficient scope
-        ;; from a client with global scope means she cannot create a
-        ;; user.
-        "alice" "global-scope-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"}
-        403
+      ;; Alice has permission, and can use a client with sufficient
+      ;; scope to create a user.
+      "alice" "read-write-app" "authorization_code" nil 200
+      "alice" "read-write-app" "implicit" nil 200
 
-        "alice" "global-scope-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"}
-        403
+      ;; Alice has permission, using a client with sufficient scope
+      ;; but requesting insufficient scope to create a user.  scope to
+      ;; create a user results in a Forbidden response.
+      "alice" "read-write-app" "authorization_code" #{"system/read"} 403
+      "alice" "read-write-app" "implicit" #{"system/read"} 403
 
-        ;; Alice has permission, and can use a client with sufficient
-        ;; scope to create a user.
-        "alice" "read-write-app" "authorization_code" nil 200
-        "alice" "read-write-app" "implicit" nil 200
+      ;; Alice has permission, using a client with sufficient scope
+      ;; and explicitly requesting sufficient scope to create a user.
+      "alice" "read-write-app" "authorization_code" #{"system/write"} 200
+      "alice" "read-write-app" "implicit" #{"system/write"} 200
 
-        ;; Alice has permission, using a client with sufficient scope
-        ;; but requesting insufficient scope to create a user.  scope to
-        ;; create a user results in a Forbidden response.
-        "alice" "read-write-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"}
-        403
+      ;; Alice has permission, but using a client with insufficient
+      ;; scope.
+      "alice" "read-only-app" "authorization_code" nil 403
+      "alice" "read-only-app" "implicit" nil 403
 
-        "alice" "read-write-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"}
-        403
+      ;; Alice has permission, but using a client with insufficient
+      ;; scope and explicitly requesting insufficient scope.
+      "alice" "read-only-app" "authorization_code" #{"system/read"} 403
+      "alice" "read-only-app" "implicit" #{"system/read"} 403
 
-        ;; Alice has permission, using a client with sufficient scope
-        ;; and explicitly requesting sufficient scope to create a user.
-        "alice" "read-write-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/write"}
-        200
+      ;; Bob can't add users, regardless of the application he uses.
+      "bob" "global-scope-app" "authorization_code" nil 403
+      "bob" "global-scope-app" "implicit" nil 403
 
-        "alice" "read-write-app" "implicit"
-        #{"https://auth.example.test/scopes/system/write"}
-        200
+      ;; Bob doesn't have permission to add users, and this means he
+      ;; can't add users even if he's able to request sufficient
+      ;; scope with a client that is configured with global scope.
+      "bob" "global-scope-app" "authorization_code" #{"system/read" "system/write"} 403
+      "bob" "global-scope-app" "implicit" #{"system/read" "system/write"} 403
 
-        ;; Alice has permission, but using a client with insufficient
-        ;; scope.
-        "alice" "read-only-app" "authorization_code" nil 403
-        "alice" "read-only-app" "implicit" nil 403
+      ;; Bob doesn't have permission to add users, and this means he
+      ;; can't add users even if he's able to request sufficient
+      ;; scope with a client that is configured with sufficient
+      ;; scope.
+      "bob" "read-write-app" "authorization_code" #{"system/read" "system/write"} 403
+      "bob" "read-write-app" "implicit" #{"system/read" "system/write"} 403
 
-        ;; Alice has permission, but using a client with insufficient
-        ;; scope and explicitly requesting insufficient scope.
-        "alice" "read-only-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"} 403
+      ;; Bob doesn't have permission to add users, using a client
+      ;; with insufficient scope.
+      "bob" "read-only-app" "authorization_code" nil 403
+      "bob" "read-only-app" "implicit" nil 403
 
-        "alice" "read-only-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"} 403
-
-        ;; Bob can't add users, regardless of the application he uses.
-        "bob" "global-scope-app" "authorization_code" nil 403
-        "bob" "global-scope-app" "implicit" nil 403
-
-        ;; Bob doesn't have permission to add users, and this means he
-        ;; can't add users even if he's able to request sufficient
-        ;; scope with a client that is configured with global scope.
-        "bob" "global-scope-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        403
-
-        "bob" "global-scope-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        403
-
-        ;; Bob doesn't have permission to add users, and this means he
-        ;; can't add users even if he's able to request sufficient
-        ;; scope with a client that is configured with sufficient
-        ;; scope.
-        "bob" "read-write-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        403
-
-        "bob" "read-write-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"
-          "https://auth.example.test/scopes/system/write"}
-        403
-
-        ;; Bob doesn't have permission to add users, using a client
-        ;; with insufficient scope.
-        "bob" "read-only-app" "authorization_code" nil 403
-        "bob" "read-only-app" "implicit" nil 403
-
-        ;; Bob doesn't have permission to add users, using a client
-        ;; with insufficient scope, explicitly request insufficient
-        ;; scope.
-        "bob" "read-only-app" "authorization_code"
-        #{"https://auth.example.test/scopes/system/read"}
-        403
-
-        "bob" "read-only-app" "implicit"
-        #{"https://auth.example.test/scopes/system/read"}
-        403)))
+      ;; Bob doesn't have permission to add users, using a client
+      ;; with insufficient scope, explicitly request insufficient
+      ;; scope.
+      "bob" "read-only-app" "authorization_code" #{"system/read"} 403
+      "bob" "read-only-app" "implicit" #{"system/read"} 403)))
