cbf45ee80556345c57ce287d387d9f5d050127f2
Malcolm Sparks <mal@juxt.pro>
2023-08-17 14:17:20 +0100

    Add failing test for recursive tx-fn exceptions
    In XTDBv1, an exception from a sub tx-fn will not be available. This
    makes debugging somewhat harder and is something that should be
    improved in due course.

--- server/test/juxt/site/tx_test.clj
+++ server/test/juxt/site/tx_test.clj
@@ -2,6 +2,9 @@
 
 (ns juxt.site.tx-test
   (:require
+   [clojure.set :as set]
+   [xtdb.db :as xt.db]
+   [xtdb.codec :refer [crux->xt]]
    [clojure.test :refer [deftest is are use-fixtures]]
    [juxt.site.logging :refer [with-logging]]
    [juxt.site.repl :as repl]
@@ -54,6 +57,70 @@
             :juxt.site.xt/message "Whoops!"
             :juxt.site.xt/ex-data {:foo "foo"}}
            (dissoc exception-doc :xt/id)))))
+
+;; Failing test - because we aren't yet able to recurse the exceptions
+;; of the sub-tx-fns. But hopefully we can do so in XTDBv2
+(comment
+  (deftest tx-exception-recursive-doc-test
+    (let [tx-ops
+          [[:xtdb.api/put {:xt/id ::fail
+                           :xt/fn '(fn [ctx message]
+                                     (throw (ex-info message {:foo "foo"})))}]
+
+           [:xtdb.api/put {:xt/id ::create-person
+                           :xt/fn '(fn [ctx eid name age op-ix]
+                                     (let [{:xtdb.api/keys [tx-id]} (xtdb.api/indexing-tx ctx)]
+                                       [[::xt/put {:xt/id eid :name name :age age}]
+                                        [::xt/put {:xt/id (str "/events/" tx-id "/" op-ix)
+                                                   :juxt.site/type "https://meta.juxt.site/types/event"
+                                                   :op-ix op-ix}]]))}]
+
+           [:xtdb.api/put {:xt/id ::increment-age
+                           :xt/fn '(fn [ctx eid op-ix]
+                                     (let [db (xtdb.api/db ctx)
+                                           entity (xtdb.api/entity db eid)
+                                           {:xtdb.api/keys [tx-id]} (xtdb.api/indexing-tx ctx)]
+                                       [[::xt/put (update entity :age inc)]
+                                        [::xt/put {:xt/id (str "/events/" tx-id "/" op-ix)
+                                                   :juxt.site/type "https://meta.juxt.site/types/event"
+                                                   :op-ix op-ix}]]))}]
+
+           [:xtdb.api/put {:xt/id ::batch
+                           :xt/fn '(fn [ctx]
+                                     [[:xtdb.api/fn ::create-person "https://example.org/users/alice" "Alice" 40 0]
+                                      [:xtdb.api/fn ::create-person "https://example.org/users/bob" "Bob" 42 1]
+                                      [:xtdb.api/fn ::increment-age "https://example.org/users/bob" 2]
+                                      [:xtdb.api/fn ::fail "Whoops!"]])}]
+
+           ]
+
+          tx1 (xt/submit-tx *xt-node* tx-ops)
+          _ (xt/await-tx *xt-node* tx1)
+          tx2 (xt/submit-tx *xt-node* [[:xtdb.api/fn ::batch]])
+          _ (xt/await-tx *xt-node* tx2)
+
+          exception-doc (->> tx1
+                             :xtdb.api/tx-id
+                             (xt-util/tx-exception-doc *xt-node*))
+
+          tx-id (:xtdb.api/tx-id tx2)]
+
+      (is exception-doc)
+
+      (let [document-store (:document-store *xt-node*)]
+          (with-open [tx-log (xt.db/open-tx-log (:tx-log *xt-node*) (dec tx-id) {})]
+            (->> tx-log
+                 iterator-seq
+                 first                    ; narrow to this tx-id
+                 :xtdb.tx.event/tx-events ; pull out events
+                 (map #(get % 2))  ; get the document-hash of each event
+                 set
+                 (xt.db/fetch-docs document-store)
+
+                 )))
+
+
+      )))
 
 (deftest tx-event-docs-test
   (let [tx-ops
