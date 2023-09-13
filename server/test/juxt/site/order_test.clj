5d15067315a20af9735991870c713e9a66061aa6
Ellis Kenyő <elken@users.noreply.github.com>
2023-09-13 15:44:56 +0100

    feat: implement endpoints for orders (#11)

---
+++ server/test/juxt/site/order_test.clj
@@ -0,0 +1,96 @@
+(ns juxt.site.order-test
+  (:require
+   [clojure.java.io :as io]
+   [clojure.test :refer [are deftest is use-fixtures]]
+   [jsonista.core :as json]
+   [juxt.site.logging :refer [with-logging]]
+   [juxt.site.repl :as repl]
+   [juxt.site.test-helpers.client :refer [*admin-token* admin-token-fixture] :as client]
+   [juxt.site.test-helpers.fixture :refer [with-fixtures]]
+   [juxt.site.test-helpers.handler :refer [*handler* handler-fixture]]
+   [juxt.site.test-helpers.init :refer [init-fixture]]
+   [juxt.site.test-helpers.oauth :refer [with-bearer-token] :as oauth]
+   [juxt.site.test-helpers.petstore :refer [install-openapi! install-petstore!]]
+   [juxt.site.test-helpers.xt :refer [system-xt-fixture]]))
+
+(def dogs [{:id 10 :name "doggie" :status "available"}
+           {:id 11 :name "cattie" :status "pending"}])
+
+(def status-id-map
+  {10 "placed"
+   0 "approved"
+   11 "delivered"})
+
+(defn seed-data-fixture []
+  (println "Seeding data")
+  (with-bearer-token *admin-token*
+    (pr-str
+     (for [dog dogs]
+       (let [payload (json/write-value-as-bytes dog json/keyword-keys-object-mapper)]
+         (*handler* {:juxt.site/uri "https://data.example.test/petstore/pet"
+                     :ring.request/method :post
+                     :ring.request/headers
+                     {"content-type" "application/json"
+                      "content-length" (str (count payload))
+                      "accept" "application/json"}
+                     :ring.request/body (io/input-stream payload)}))))))
+
+(defn install-openapi-fixture [f]
+  (install-openapi!)
+  (install-petstore!)
+  (seed-data-fixture)
+  (f))
+
+(use-fixtures :each system-xt-fixture handler-fixture init-fixture admin-token-fixture install-openapi-fixture)
+
+(defn get-order [id]
+  (*handler* {:juxt.site/uri (str "https://data.example.test/petstore/store/order/" id)
+              :ring.request/method :get}))
+
+(defn delete-order [id]
+  (*handler* {:juxt.site/uri (str "https://data.example.test/petstore/store/order/" id)
+              :ring.request/method :delete}))
+
+(defn place-order [id]
+  (let [body {:shipDate (java.time.Instant/now)
+              :complete true
+              :petId id
+              :status (status-id-map id)
+              :id id
+              :quantity 1}
+        payload (json/write-value-as-bytes body json/keyword-keys-object-mapper)]
+    (*handler* {:juxt.site/uri "https://data.example.test/petstore/store/order"
+                :ring.request/method :post
+                :ring.request/headers
+                {"content-type" "application/json"
+                 "content-length" (str (count payload))
+                 "accept" "application/json"}
+                :ring.request/body (io/input-stream payload)})))
+
+(defn get-inventory []
+  (*handler* {:juxt.site/uri "https://data.example.test/petstore/store/inventory"
+              :ring.request/method :get}))
+
+(deftest should-fail-missing-pet-test
+  (with-bearer-token *admin-token*
+    (let [{:ring.response/keys [status]} (place-order 0)]
+      (is (= 404 status)))))
+
+(deftest should-get-valid-order-test
+  (with-bearer-token *admin-token*
+    (let [{:ring.response/keys [status]} (place-order (-> dogs first :id))]
+      (is (= 200 status (:ring.response/status (get-order (-> dogs first :id))))))))
+
+(deftest should-delete-order-test
+  (with-bearer-token *admin-token*
+    (let [{:ring.response/keys [status]} (place-order (-> dogs first :id))]
+      (is (= 200 status (:ring.response/status (delete-order (-> dogs first :id))))))))
+
+(deftest should-show-inventory-test
+  (with-bearer-token *admin-token*
+    (doall
+     (for [{:keys [id]} dogs]
+       (place-order id)))
+    (is (= {"placed" 1
+            "delivered" 1}
+           (:ring.response/body (get-inventory))))))
---
+++ server/test/juxt/site/test_helpers/petstore.clj
@@ -0,0 +1,54 @@
+(ns juxt.site.test-helpers.petstore
+  (:require
+   [jsonista.core :as json]
+   [clojure.java.io :as io]
+   [juxt.site.test-helpers.client :refer [admin-token-fixture *admin-token*] :as client]
+   [juxt.site.test-helpers.oauth :refer [with-bearer-token] :as oauth]
+   [clojure.walk :refer [postwalk]]
+   [juxt.site.install.common-install-util :as ciu]
+   [juxt.site.test-helpers.init :refer [init-fixture CONFIG]]
+   [juxt.site.test-helpers.local-files-util :refer [install-bundles!]]))
+
+(defn install-openapi! []
+  (install-bundles!
+   [;; This installs the operation that allows us to install an OpenAPI
+    ["juxt/site/openapis-api" {}]]
+   (get CONFIG "uri-map")))
+
+(defn install-petstore! []
+  (let [openapi-file (io/file "../demo/petstore/openapi.json")
+        openapi (json/read-value (slurp openapi-file))
+        data-base-uri (get-in CONFIG ["uri-map" "https://data.example.org"])
+
+        _
+        (with-bearer-token *admin-token*
+          (let [mapped-openapi (update-in openapi ["servers" 0 "url"] #(str data-base-uri %))]
+            (-> (postwalk (ciu/make-uri-map-replace-walk-fn
+                           (get CONFIG "uri-map"))
+                          mapped-openapi)
+                client/install-openapi!)))]
+    (install-bundles!
+     ["juxt/site/system-api-openapi"
+      "juxt/site/login-form"
+      "juxt/site/password-based-user-identity"
+      "juxt/site/example-users"
+      ["juxt/site/keypair" {"kid" "test-kid"}]
+      ["juxt/site/sessions" {}]
+      ["juxt/site/oauth-authorization-endpoint"
+       {"session-scope" "https://auth.example.test/session-scopes/form-login-session"
+        "authorization-code-length" 12
+        "jti-length" 12}]
+      "juxt/site/oauth-token-endpoint"
+      "demo/petstore/operations"
+      ;; Alice is a Petstore Owner
+      ["juxt/site/user-role-assignment"
+       {"username" "alice"
+        "rolename" "SiteAdmin"}]
+      ["juxt/site/user-role-assignment"
+       {"username" "alice"
+        "rolename" "PetstoreOwner"}]
+      "juxt/site/system-test-clients"
+      ["juxt/site/application-role-assignment"
+       {"clientid" "site-cli"
+        "rolename" "SiteSystemQuery"}]]
+     (get CONFIG "uri-map"))))
