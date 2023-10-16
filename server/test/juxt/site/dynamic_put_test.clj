bcbb139858b5ffa1f92bb8acf29ca30901e6aae5
Malcolm Sparks <mal@juxt.pro>
2023-10-16 11:09:41 +0200

    Improve dynamic-put-test

--- server/test/juxt/site/dynamic_put_test.clj
+++ server/test/juxt/site/dynamic_put_test.clj
@@ -12,12 +12,14 @@
    [juxt.site.test-helpers.init :refer [init-fixture]]
    [juxt.site.test-helpers.client :refer [admin-token-fixture *admin-token*]]
    [juxt.site.repl :as repl]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]
+   [jsonista.core :as json]))
 
 (defn dynamic-remote-bundles []
   (install-bundles!
-   ["juxt/site/operations-api"]
-   ["juxt/site/full-dynamic-remote"]
+   ["juxt/site/operations-api"
+    "juxt/site/full-dynamic-remote"]
    RESOURCE_SERVER))
 
 (defn dynamic-remote-bundles-fixture [f]
@@ -34,39 +36,93 @@
 (with-fixtures
   (repl/ls)
   (repl/e "https://data.example.test/_site/meta-resource")
+
+  (repl/ls)
+
   (with-bearer-token *admin-token*
 
     ;; Create resource
-    (with-request-body (pr-str {})
+    (with-request-body (pr-str {:message "Hi"})
       (*handler*
        {:juxt.site/uri "https://data.example.test/contacts.meta"
         :ring.request/method :put
         :ring.request/headers {"content-type" "application/edn"}}))
 
+    ;; Create operation
+    ;; https://data.example.test/operations/add-contact
+    (with-request-body
+      (pr-str
+       {:xt/id "https://data.example.test/operations/add-contact"
+        :juxt.site/prepare
+        {:juxt.site.sci/program {}}
+        :juxt.site/transact
+        {:juxt.site.sci/program
+         (pr-str
+          [[:xtdb.api/put {:xt/id "https://data.example.test/contacts/fred"}]])}
+        :juxt.site/rules
+        '[[(allowed? subject operation resource permission)
+           [permission :juxt.site/user "alice"]]]})
+      (*handler*
+       {:juxt.site/uri "https://data.example.test/_site/operations"
+        :ring.request/method :post
+        :ring.request/headers {"content-type" "application/edn"}}))
+
+    ;; Create permission to call operation
+    (with-request-body
+      (pr-str
+       ;; TODO: We should be careful not to allow existing permissions
+       ;; to be overwritten. Perhaps they must first be revoked with
+       ;; requesting a DELETE method on a permission. But who can do
+       ;; this? Perhaps the granter of a permission must be recorded
+       ;; with the permission, to ensure that only the granter can
+       ;; revoke.
+       {:xt/id "https://data.example.test/permissions/add-contact"
+        :juxt.site/operation "https://data.example.test/operations/add-contact"
+        :juxt.site/purpose nil
+        :juxt.site/user "alice"})
+      (*handler*
+       {:juxt.site/uri "https://data.example.test/_site/permissions"
+        :ring.request/method :post
+        :ring.request/headers {"content-type" "application/edn"}}))
+
     ;; Attach PUT method
     (with-request-body
       (pr-str
-       [
-        [:add-method
-         {:method "PUT"
-          :operation "https://data.example.test/operations/add-contact"
-          }]])
+       [[:add-method
+         {:method "POST"
+          :operation "https://data.example.test/operations/add-contact"}]])
       (*handler*
        {:juxt.site/uri "https://data.example.test/contacts.meta"
         :ring.request/method :patch
-        :ring.request/headers {"content-type" "application/edn"}}))
+        :ring.request/headers {"content-type" "application/edn"}})))
 
-    ;; Create a get-contact operation. First, create an operations-api
-    ;; which lists all the operations I have access to, and allows me
-    ;; to create new ones (if I have access to create-operation)
+  (repl/e "https://data.example.test/contacts")
+  (repl/e "https://data.example.test/operations/add-contact")
 
-    ;; POST to the create-operation resource?
+  #_(repl/ls)
 
-    )
+  ;; Test it
+  (with-request-body
+    (pr-str
+     {})
+    (*handler*
+     {:juxt.site/uri "https://data.example.test/contacts"
+      :ring.request/method :post
+      :ring.request/headers {"content-type" "application/edn"}}))
 
-  #_(repl/e "https://data.example.test/_site/meta-resource")
-  (repl/e "https://data.example.test/contacts"))
+  (repl/e "https://data.example.test/contacts/fred")
 
+  ;; What is the Allow response header from an OPTIONS request?
 
-;; TODO: Filter out unallowed methods from OPTIONS and the Allow
-;; response header.
+  ;; TODO: Filter out unallowed methods from OPTIONS and the Allow
+  ;; response header.
+
+  (let [response
+        (*handler*
+         {:juxt.site/uri "https://data.example.test/contacts.meta"
+          :ring.request/method :options})]
+    (select-keys
+     response
+     [:ring.response/status
+      :ring.response/headers
+      :ring.response/body])))
